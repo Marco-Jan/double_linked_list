1e4f0dbf34bb0193a36e14fa7df8c72da4c69ab3
Baloou <127010557+Marco-Jan@users.noreply.github.com>
2023-12-21 09:03:46 +0100

    add unshift, get,set, insert, remove method

--- main.ts
+++ main.ts
@@ -65,10 +65,10 @@ class doubleLinkedList<T> {
 
         const currentHeadNote = this.head;
 
-        if(this.length === 1){
+        if (this.length === 1) {
             this.head = null;
             this.tail = null;
-        }else {
+        } else {
             this.head = this.head!.next;
             if (this.head) {
                 this.head.prev = null;
@@ -82,7 +82,84 @@ class doubleLinkedList<T> {
 
         return currentHeadNote;
 
-        
+
+    }
+
+    unshift(value: T) {
+        const newNode = new ListNode(value);
+    
+        if (this.head === null) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.head.prev = newNode;
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+    
+        this.length++;
+        return this;
+    }
+
+    get(index: number) {
+        if (index < 0 || index >= this.length) return null;
+    
+        let current = this.head;
+        for (let i = 0; i < index; i++) {
+            if (current) {
+                current = current.next;
+            }
+        }
+    
+        return current;
+    }
+    
+    set(index: number, value: T) {
+        const node = this.get(index);
+        if (node) {
+            node.value = value;
+            return true;
+        }
+    
+        return false;
+    }
+
+    insert(index: number, value: T) {
+        if (index < 0 || index > this.length) return false;
+        if (index === 0) return !!this.unshift(value);
+        if (index === this.length) return !!this.append(value);
+    
+        const newNode = new ListNode(value);
+        const beforeNode = this.get(index - 1);
+        const afterNode = beforeNode!.next;
+    
+        beforeNode!.next = newNode;
+        newNode.prev = beforeNode;
+        newNode.next = afterNode;
+        afterNode!.prev = newNode;
+    
+        this.length++;
+        return true;
+    }
+    
+    remove(index: number) {
+        if (index < 0 || index >= this.length) return null;
+        if (index === 0) return this.shift();
+        if (index === this.length - 1) return this.pop();
+    
+        const removedNode = this.get(index);
+        const beforeNode = removedNode!.prev;
+        const afterNode = removedNode!.next;
+    
+        beforeNode!.next = afterNode;
+        afterNode!.prev = beforeNode;
+        removedNode!.next = null;
+        removedNode!.prev = null;
+    
+        this.length--;
+        return removedNode;
+    }
+
 }
 
 
